36306ba80722f712fe622aabe0b7d3838d97dd28
mofmof721 <morishima0721@gmail.com>
2018-04-13 01:07:51 +0900

    修正

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,7 @@
 package controllers;
 
-import java.util.List;
-
-import models.UserMaster;
 import play.mvc.Controller;
 import play.mvc.Result;
-
 import views.html.index;
 
 /**
@@ -20,12 +16,6 @@ public class Application extends Controller {
 	 * @return 処理結果
 	 */
 	public Result index() {
-		// ユーザーマスタを検索する
-		List<UserMaster> userMasterList = UserMaster.getFind().all();
-		for (UserMaster data : userMasterList) {
-			// 利用者名を出力する
-			System.out.println(data.getUserName());
-		}
 		// index画面を表示する
 		return ok(index.render("Your new application is ready."));
 	}
